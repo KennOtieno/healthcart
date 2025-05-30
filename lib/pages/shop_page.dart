e56a1497ded57f0243110afc732128f006df3188
KennOtieno <otiken001@gmail.com>
2025-05-30 05:25:28 -0700

    Debugging

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -5,10 +5,56 @@ class ShopPage extends StatefulWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Shop Page'
-      ),
+    return Column(
+      children: [
+        // A Search Bar
+       Container(
+            margin: const EdgeInsets.all(10),  
+            padding: const EdgeInsets.symmetric(horizontal: 10),  
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                Icon(Icons.search, color: Colors.black),
+              ],
+            )
+          ),
+
+        // Most Purchased
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Most Purchased',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'See All',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue[700],
+                ),
+              )
+          
+            ]
+          ),
+        )
+      ]
     )
   }
 }

525e816aee2f9d9295fc1c11efb47c79ece3db54
KennOtieno <otiken001@gmail.com>
2025-06-03 07:32:16 -0700

    New

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,122 +11,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Int to keep track of selected index so as to control the Bottom Navigation
 
-  // A Function to update selected index
+  // This Selected Index is to control bottom navigattion starting with 0 when user taps on bottom bar
+  int _selectedIndex = 0;
 
-  // When user taps on button
-  void navigateBottomNavigation(int index) {
-    // Updating state with new index
-    setState( (){
+  // This Func updates the selected index when user taps on bottom bar
+  void navigateBottomBar(int index) {
+    setState( () {
       _selectedIndex = index;
     });
   }
 
-  // Pages to be displayed on the Home Page
-  // Shop Page
-  // ignore: non_constant_identifier_names
-  ShopPage()
+  // Pages o display
+  final List<Widget> _pages = [
+    // Shop Page
+    const ShopPage(),
 
-  // Cart Page
-  // ignore: non_constant_identifier_names
-  CartPage()
-  
+    // Cart Page
+    const CartPage(),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-      bottomNavigationBar: BottomNavigation(
-        onTabChange: (index) => navigateBottomNavigation(index),
+      backgroundColor: Colors.white,
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
       ),
+
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Builder(
           builder: (context) {
             return IconButton(
-              icon: const Icon(Icons.menu, color: Colors.black),
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+                ),
               onPressed: () {
-                Scaffold.of(context).openDrawer();
+                Scaffold.of(context).openDrawer(); // Opens the drawer when menu icon is tapped
               },
             );
-          },
+          }
         ),
       ),
-      drawer: Drawer(
-        backgroundColor: Colors.grey[100],
+      // A Blank Drawer
+      drawer: const Drawer(
+        backgroundColor: Colors.grey,
         child: Column(
-          children: [
-            // Logo
-            DrawerHeader(
-              child: Image.asset(
-                'lib/images/healthcart_logo.png',
-                color: Colors.black,
-              ),
-            ),
-            // Divider
-            const Padding(
-              padding: EdgeInsets.symmetric(24),
-              child: Divider(
-                color: Colors.grey,
-              ),
-            ),
-
-            // Other Pages
-            // 1. Shop Page
-            const Padding(
-              padding: EdgeInsets.only(left: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.home,
-                   color: Colors.white
-                   ),
-                title: Text(
-                  'Home', 
-                  style: TextStyle(
-                    color: Colors.white
-                    ),
-                    ),
-              ),
-            ),
-
-            // 2. About Page
-            const Padding(
-              padding: EdgeInsets.only(left: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.info,
-                   color: Colors.white
-                   ),
-                title: Text(
-                  'About', 
-                  style: TextStyle(
-                    color: Colors.white
-                    ),
-                    ),
-              ),
-            ),
-
-            // 3. LogOut Page
-            const Padding(
-              padding: EdgeInsets.only(left: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.logout,
-                   color: Colors.white
-                   ),
-                title: Text(
-                  'Log Out', 
-                  style: TextStyle(
-                    color: Colors.white
-                    ),
-                    ),
-              ),
-            ),
+          children: [            
           ],
         ),
       ),
-      body: _pages[_selectedIndex],
+      body: _pages[_selectedIndex], // Displaying the selected page based on index
     );
   }
 }
